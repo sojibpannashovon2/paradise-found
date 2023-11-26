3bd64fc7622455f68de1bb96f1d1cb66dce88483
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2023-11-26 18:07:31 +0600

    user saved  to database

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-const port = process.env.PORT || 7000
+const port = process.env.PORT || 12000
 
 // middleware
 const corsOptions = {
@@ -14,22 +14,41 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mq0mae1.mongodb.net/?retryWrites=true&w=majority`
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yaanftr.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
       serverApi: {
             version: ServerApiVersion.v1,
             strict: true,
             deprecationErrors: true,
-      },
-})
+      }
+});
 
 async function run() {
       try {
-            const usersCollection = client.db('aircncDb').collection('users')
-            const roomsCollection = client.db('aircncDb').collection('rooms')
-            const bookingsCollection = client.db('aircncDb').collection('bookings')
+            const usersCollection = client.db('paradiseDb').collection('users')
+            const roomsCollection = client.db('paradiseDb').collection('rooms')
+            const bookingsCollection = client.db('paradiseDb').collection('bookings')
 
+
+            //save user to database
+
+            app.put("/users/:email", async (req, res) => {
+                  const email = req.params.email
+                  const user = req.body
+                  const query = { email: email }
+                  const options = { upsert: true }
+
+                  const updateDoc = {
+                        $set: user,
+                  }
+
+                  const result = await usersCollection.updateOne(query, updateDoc, options)
+
+                  console.log(result);
+                  res.send(result)
+            })
             // Send a ping to confirm a successful connection
             await client.db('admin').command({ ping: 1 })
             console.log(
@@ -43,9 +62,9 @@ async function run() {
 run().catch(console.dir)
 
 app.get('/', (req, res) => {
-      res.send('AirCNC Server is running..')
+      res.send('Paradise Found Server is running..')
 })
 
 app.listen(port, () => {
-      console.log(`AirCNC is running on port ${port}`)
+      console.log(`Paradise Found is running on port ${port}`)
 })
