d19896d9bc2863f5a1c0aa7d079eea19e88ad81e
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2024-11-27 00:48:57 +0600

    google map add

--- client/src/Pages/Dashboard/AddRoom.jsx
+++ client/src/Pages/Dashboard/AddRoom.jsx
@@ -1,107 +1,104 @@
-import React, { useContext, useState } from 'react';
-import AddRoomForm from '../../component/Forms/AddRoomForm';
-import { uploadImage } from '../../Api/utils';
-import { AuthContext } from '../../providers/AuthProvider';
+import React, { useContext, useState } from "react";
+import AddRoomForm from "../../component/Forms/AddRoomForm";
+import { uploadImage } from "../../Api/utils";
+import { AuthContext } from "../../providers/AuthProvider";
 
-import toast from 'react-hot-toast';
-import { addRoom } from '../../Api/rooms';
-import { useNavigate } from 'react-router-dom';
+import toast from "react-hot-toast";
+import { addRoom } from "../../Api/rooms";
+import { useNavigate } from "react-router-dom";
 
 const AddRoom = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [uploadButtonText, setUploadButtonText] = useState(`Upload Image`);
 
-      const [loading, setLoading] = useState(false)
-      const navigate = useNavigate()
-      const [uploadButtonText, setUploadButtonText] = useState(`Upload Image`)
+  const [dates, setDates] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
 
-      const [dates, setDates] = useState({
-            startDate: new Date(),
-            endDate: new Date(),
-            key: 'selection',
+  const handleDates = (ranges) => {
+    setDates(ranges.selection);
+  };
+
+  const { user } = useContext(AuthContext);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const location = event.target.location.value;
+    const title = event.target.title.value;
+
+    const from = dates.startDate;
+    const to = dates.endDate;
+
+    const price = event.target.price.value;
+    const total_guest = event.target.total_guest.value;
+
+    const bedrooms = event.target.bedrooms.value;
+    const bathrooms = event.target.bathrooms.value;
+
+    const description = event.target.description.value;
+    const category = event.target.category.value;
+
+    const image = event.target.image.files[0];
+    setUploadButtonText(`Uploadding.....`);
+    uploadImage(image)
+      .then((data) => {
+        // console.log(data.data.display_url)
+        const roomData = {
+          image: data.data.display_url,
+          host: {
+            name: user?.displayName,
+            image: user?.photoURL,
+            email: user?.email,
+          },
+          location,
+          from,
+          to,
+          title,
+          price,
+          total_guest,
+          bathrooms,
+          bedrooms,
+          description,
+          category,
+        };
+        console.log(roomData);
+        //save room to dataBase
+        addRoom(roomData)
+          .then((data) => {
+            console.log(data);
+            setUploadButtonText(`Uploaded`);
+            setLoading(false);
+            toast.success(`Room data is added successfully !!!`);
+            navigate(`/dashboard/my-listings`);
+          })
+          .catch((err) => {
+            console.log(err.message);
+          });
       })
+      .catch((err) => {
+        console.log(err.message);
+        setLoading(false);
+      });
+    // console.log(location);
+  };
 
-      const handleDates = ranges => {
+  const handleImageChange = (image) => {
+    setUploadButtonText(image.name);
+  };
 
-            setDates(ranges.selection)
-      }
-
-      const { user } = useContext(AuthContext);
-      const handleSubmit = (event) => {
-            event.preventDefault();
-            setLoading(true)
-            const location = event.target.location.value
-            const title = event.target.title.value
-
-            const from = dates.startDate
-            const to = dates.endDate
-
-            const price = event.target.price.value
-            const total_guest = event.target.total_guest.value
-
-            const bedrooms = event.target.bedrooms.value
-            const bathrooms = event.target.bathrooms.value
-
-            const description = event.target.description.value
-            const category = event.target.category.value
-
-            const image = event.target.image.files[0]
-            setUploadButtonText(`Uploadding.....`)
-            uploadImage(image)
-                  .then(data => {
-                        // console.log(data.data.display_url)
-                        const roomData = {
-                              image: data.data.display_url,
-                              host: {
-                                    name: user?.displayName,
-                                    image: user?.photoURL,
-                                    email: user?.email,
-                              },
-                              location,
-                              from,
-                              to,
-                              title,
-                              price,
-                              total_guest,
-                              bathrooms,
-                              bedrooms,
-                              description,
-                              category,
-
-                        }
-                        console.log(roomData);
-                        //save room to dataBase
-                        addRoom(roomData)
-                              .then(data => {
-                                    console.log(data)
-                                    setUploadButtonText(`Uploaded`)
-                                    setLoading(false);
-                                    toast.success(`Room data is added successfully !!!`)
-                                    navigate(`/dashboard/my-listings`)
-                              })
-                              .catch(err => {
-                                    console.log(err.message);
-                              })
-
-                  })
-                  .catch(err => {
-                        console.log(err.message);
-                        setLoading(false);
-                  })
-            // console.log(location);
-
-      }
-
-      const handleImageChange = (image) => {
-            setUploadButtonText(image.name)
-      }
-
-      return <AddRoomForm
-            handleSubmit={handleSubmit}
-            loading={loading}
-            handleImageChange={handleImageChange}
-            uploadButtonText={uploadButtonText}
-            dates={dates}
-            handleDates={handleDates}
-      />
+  return (
+    <AddRoomForm
+      handleSubmit={handleSubmit}
+      loading={loading}
+      handleImageChange={handleImageChange}
+      uploadButtonText={uploadButtonText}
+      dates={dates}
+      handleDates={handleDates}
+    />
+  );
 };
 
 export default AddRoom;
--- client/src/Pages/RoomDetails/RoomDetails.jsx
+++ client/src/Pages/RoomDetails/RoomDetails.jsx
@@ -66,7 +66,20 @@ const RoomDetails = () => {
             <ScrollingImages roomData={roomData} />
           </div>
 
-          <ReviewRoom />
+          <ReviewRoom roomData={roomData} />
+
+          <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d40875.8173323748!2d92.25224206160642!3d20.848249202547695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30ae1050b22de32d%3A0xbcc6773394cd8c5b!2sTeknaf%20Beach!5e0!3m2!1sen!2sbd!4v1732644668417!5m2!1sen!2sbd"
+              width="600"
+              height="450"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              title="Teknaf Beach Map"
+            ></iframe>
+          </div>
         </div>
       </div>
     </Container>
